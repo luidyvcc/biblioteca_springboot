ce43ef80f43d330d691f1aea70292cbf449dee1f
Luidy Vaccaro <ldy80@outlook.com>
2019-06-08 17:00:37 -0300

    aula 01 finalizada

--- src/main/java/br/biblioteca/livros/controllers/AutorController.java
+++ src/main/java/br/biblioteca/livros/controllers/AutorController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.biblioteca.livros.entidades.Livro;
+import br.biblioteca.livros.entidades.Autor;
 
 @Controller
-@RequestMapping("/livros")
-public class LivroController {
+@RequestMapping("/autores")
+public class AutorController {
+
+private String template;
 	
-	private String template;
-	
-	public LivroController() {
-		this.template = "/livros";
+	public AutorController() {
+		this.template = "/autores";
 	}
 	
 	@GetMapping("/list")
@@ -30,22 +30,21 @@ public class LivroController {
 	}
 	
 	@PostMapping("/gravar")
-	public ModelAndView storage(Livro livro) {
-		System.out.println("A: "+livro.hashCode());
-		System.out.println("Livro gravado: "+livro.getNome()+" qtde: "+livro.getQuantidadePaginas());
-		return new ModelAndView("redirect:/"+this.template+"/list");
+	public ModelAndView storage(Autor autor) {
+		System.out.println("Autor gravado: "+autor.getNome());
+		return new ModelAndView("redirect:/autores/list");
 	}
 	
 	@GetMapping("/editar/{id}")
 	public ModelAndView edit(@PathVariable("id") Long id) {	
-		System.out.println("Editar livro: "+id);
+		System.out.println("Editar autor: "+id);
 		return new ModelAndView(this.template+"/edit");
 	}
 	
 	@GetMapping("/excluir/{id}")
 	public ModelAndView destroy(@PathVariable("id") Long id) {
-		System.out.println("Excluir livro: "+id);
-		return new ModelAndView("redirect:/"+this.template+"/list");
+		System.out.println("Excluir autor: "+id);
+		return new ModelAndView("redirect:/autores/list");
 	}
 	
 }
